a18108267a29c6e71723fa265bb5bd55b7457faa
Henry Wang <henrymw@vt.edu>
2019-08-14 22:49:35 -0400

    Updates

---
+++ precache-manifest.5da4dfab40a877f950bf9ad31f7e41ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c439f8d4caeb2aee227cae52f730683b",
+    "url": "/react-d3-barchart/index.html"
+  },
+  {
+    "revision": "583461e3684e526822c6",
+    "url": "/react-d3-barchart/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "a803055be3524ca435ad",
+    "url": "/react-d3-barchart/static/css/main.839a7fac.chunk.css"
+  },
+  {
+    "revision": "583461e3684e526822c6",
+    "url": "/react-d3-barchart/static/js/2.8bbf9a4e.chunk.js"
+  },
+  {
+    "revision": "a803055be3524ca435ad",
+    "url": "/react-d3-barchart/static/js/main.78396036.chunk.js"
+  },
+  {
+    "revision": "33a166632cbd0a401f60",
+    "url": "/react-d3-barchart/static/js/runtime~main.c56fba6c.js"
+  }
+]);
